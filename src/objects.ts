d373f7c262a77d26237843c07f17723e8dd21f7f
KAREENA <KAREENAK@UDEL.EDU>
2023-02-28 14:26:56 -0500

    finished 8 functions

--- src/objects.ts
+++ src/objects.ts
@@ -129,7 +129,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const new_question = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return new_question;
 }
 
 /**
