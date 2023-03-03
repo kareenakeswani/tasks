25e53fcc538ec3e15100306f3e5a73dfe53761b9
KAREENA <KAREENAK@UDEL.EDU>
2023-03-03 16:28:28 -0500

    finished 4 functions

--- src/nested.ts
+++ src/nested.ts
@@ -46,7 +46,10 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    const new_questions = questions.filter(
+        (question: Question): boolean => question.id !== id
+    );
+    return new_questions;
 }
 
 /***
