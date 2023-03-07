f0220845ddd3890b413d2dea89472ce2e40f1cf5
KAREENA <KAREENAK@UDEL.EDU>
2023-03-07 11:08:54 -0500

    finished components

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,36 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday = "💗" | "🎆" | "🪔" | "🦃" | "🎄";
+const ALPHABET_TRANSITIONS: Record<Holiday, Holiday> = {
+    "🎄": "🪔",
+    "🪔": "🎆",
+    "🎆": "🦃",
+    "🦃": "💗",
+    "💗": "🎄"
+};
+const CHRONOLOGICAL_TRANSITIONS: Record<Holiday, Holiday> = {
+    "💗": "🎆",
+    "🎆": "🪔",
+    "🪔": "🦃",
+    "🦃": "🎄",
+    "🎄": "💗"
+};
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("💗");
+    function changeHolidayAlphabet(): void {
+        const newHoliday = ALPHABET_TRANSITIONS[holiday];
+        setHoliday(newHoliday);
+    }
+    function changeHolidayYear(): void {
+        const newHoliday = CHRONOLOGICAL_TRANSITIONS[holiday];
+        setHoliday(newHoliday);
+    }
+    return (
+        <div>
+            <span>Holiday: {holiday}</span>
+            <Button onClick={changeHolidayAlphabet}>Advance by Alphabet</Button>
+            <Button onClick={changeHolidayYear}>Advance by Year</Button>
+        </div>
+    );
 }
