065efe441e7d7b2c3481f05b8a9798cc8b51e219
KAREENA <KAREENAK@UDEL.EDU>
2023-03-07 21:52:16 -0500

    finished colored box yayayayayayyayayayayayayy

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,42 +4,32 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): JSX.Element {
+export function ColoredBox(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     function settingColorIndex() {
-        setColorIndex(1 + DEFAULT_COLOR_INDEX);
+        setColorIndex(1 + colorIndex);
         if (colorIndex === 2) {
             setColorIndex(0);
         }
     }
-    return <Button onClick={() => settingColorIndex()}>Next Color</Button>;
-}
-
-function ColorPreview() {
-    return (
-        <div
-            data-testid="colored-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: "5px"
-            }}
-        ></div>
-    );
-}
-
-export function ColoredBox(): JSX.Element {
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <Button onClick={() => settingColorIndex()}>Next Color</Button>;
             </div>
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: COLORS[colorIndex],
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: "5px"
+                }}
+            ></div>
         </div>
     );
 }
