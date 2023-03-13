992f2b71cb716ea8256856aa7c0dc5be3f73aade
KAREENA <KAREENAK@UDEL.EDU>
2023-03-13 11:42:56 -0400

    started give attempts, made the two states

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<number>(0);
     return (
         <div>
             <h3>Give Attempts</h3>
